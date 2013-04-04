7086f29e634b3db929c859bd6fa5072d4833e5a6
Chaobin,Zhang <zhchbin@gmail.com>
2013-04-04 15:23:44 +0800

    Update main.cpp

--- Homework_3/problem/src/main.cpp
+++ Homework_3/problem/src/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
   FileInterfaceImpl f("file.out");
   int counter = 0;
   while (f.GetNext()) {
-    std::cout << counter << " "<< f.word() << std::endl;
+    std::cout << counter++ << " "<< f.word() << std::endl;
   }
 
   f.SaveTo("my_file.txt"); // This will save the words to file |my_file.txt|.
